f6ddb8863d50d9e4c255ef56851366bd92fe79e8
cuining <cncc0824@gmail.com>
2018-05-02 14:18:25 +0800

    feat(src/components): add badge & flex

--- site/pages/button/index.tsx
+++ site/pages/button/index.tsx
@@ -1,12 +1,16 @@
 import * as React from 'react'
 import { Button } from '../../../src'
+import { Flex } from '../../../src'
+import { Badge } from '../../../src'
 
 export default class Page extends React.Component {
   render() {
     return (
       <React.Fragment>
         <Button size="sm">购买授权</Button>
-        <Button size="sm" disabled={true}>购买授权</Button>
+        <Button size="sm" disabled={true}>
+          购买授权
+        </Button>
         <Button>购买授权</Button>
         <Button size="md">购买授权</Button>
         <Button disabled={true}>购买授权</Button>
@@ -22,6 +26,11 @@ export default class Page extends React.Component {
         </Button>
         <Button icon="close">button</Button>
         <Button icon="close" loading={true} />
+        <Flex type="section">
+          <div>123</div>
+          <Badge>123</Badge>
+          <Badge pill={true}>123</Badge>
+        </Flex>
       </React.Fragment>
     )
   }
