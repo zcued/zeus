b4ec919bdfb0739bde9ba9354390e0e1f97d8d95
崔宁 <cncc0824@126.com>
2018-05-03 18:23:33 +0800

    feat(src/components): update collapse (#30)

--- src/collapse/collapseItem.tsx
+++ src/collapse/collapseItem.tsx
@@ -9,11 +9,13 @@ const CollapseItemStyled = styled.div`
 `
 
 const CollapseItemTitile = styled.div`
+  display: flex;
+  justify-content: space-between;
   margin: 0 32px;
   padding: 24px 8px;
-  font-size: ${({theme}) => theme.font.size.md}px;
-  line-height: 28px ;
-  border-bottom: 2px solid ${({theme}) => theme.palette.daisy};
+  font-size: ${({ theme }) => theme.font.size.md}px;
+  line-height: 28px;
+  border-bottom: 2px solid ${({ theme }) => theme.palette.daisy};
   cursor: pointer;
 
   &.active {
@@ -30,18 +32,19 @@ interface Props {
   className?: string
   title: string | JSX.Element
   isActive?: boolean
-  name?: string
   onClick?: any
+  icon?: Function
 }
 
 export default class CollapseItem extends React.Component<Props, {}> {
   render() {
-    const { className, title, isActive, name, onClick, children } = this.props
+    const { className, title, isActive, onClick, icon, children } = this.props
 
     return (
       <CollapseItemStyled className={className}>
-        <CollapseItemTitile  className={isActive ? 'active' : ''} onClick={() => onClick(name)}>
+        <CollapseItemTitile className={isActive ? 'active' : ''} onClick={onClick}>
           {title}
+          {icon ? icon(isActive) : null}
         </CollapseItemTitile>
         <CollapseTransition isShow={isActive}>
           <CollapseItemContainer>{children}</CollapseItemContainer>
