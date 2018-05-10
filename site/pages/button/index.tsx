2dd1f7398e194ae26e5ac45c0ade035695c56c05
Ma Ning <maning0809@gmail.com>
2018-05-10 10:54:11 +0800

    update: button (#36)

--- site/pages/button/index.tsx
+++ site/pages/button/index.tsx
@@ -7,25 +7,30 @@ export default class Page extends React.Component {
   render() {
     return (
       <React.Fragment>
-        <Button size="sm">购买授权</Button>
-        <Button size="sm" disabled={true}>
-          购买授权
-        </Button>
-        <Button>购买授权</Button>
-        <Button size="md">购买授权</Button>
-        <Button disabled={true}>购买授权</Button>
-        <Button primary={true} size="sm">
-          button
-        </Button>
-        <Button primary={true}>button</Button>
-        <Button primary={true} size="md">
-          button
-        </Button>
-        <Button primary={true} disabled={true}>
-          button
-        </Button>
-        <Button icon="close">button</Button>
-        <Button icon="close" loading={true} />
+        <div style={{ background: '#fff' }}>
+          <Button icon="design" loading={true} size="sm">购买授权</Button>
+          <Button icon="design" loading={true} size="sm" disabled={true}>购买授权</Button>
+          <Button icon="design" loading={true}>购买授权</Button>
+          <Button loading={true} disabled={true}>购买授权</Button>
+          <Button loading={true} size="md">购买授权</Button>
+          <Button loading={true} size="md" disabled={true}>购买授权</Button>
+        </div>
+        <div style={{ margin: '20px' }}>
+          <Button icon="design" size="sm" type="primary">购买授权</Button>
+          <Button icon="design" size="sm" type="primary" disabled={true}>购买授权</Button>
+          <Button icon="design" type="primary">购买授权</Button>
+          <Button icon="design" type="primary" disabled={true}>购买授权</Button>
+          <Button icon="design" size="md" type="primary">购买授权</Button>
+          <Button icon="design" size="md" type="primary" disabled={true}>购买授权</Button>
+        </div>
+        <div style={{ background: '#000' }}>
+          <Button icon="design" size="sm" type="ghost">购买授权</Button>
+          <Button icon="design" size="sm" type="ghost" disabled={true}>购买授权</Button>
+          <Button icon="design" type="ghost">购买授权</Button>
+          <Button icon="design" type="ghost" disabled={true}>购买授权</Button>
+          <Button icon="design" size="md" type="ghost">购买授权</Button>
+          <Button icon="design" size="md" type="ghost" disabled={true}>购买授权</Button>
+        </div>
         <Flex type="section">
           <div>123</div>
           <Badge>123</Badge>
--- src/button/index.tsx
+++ src/button/index.tsx
@@ -5,9 +5,8 @@ import Icon from '../icon'
 import { hexa } from '../globals'
 
 interface Props {
-  primary?: boolean
+  type?: 'primary' | 'ghost'
   loading?: boolean
-  ghost?: boolean
   disabled?: boolean
   icon?: string
   size?: 'sm' | 'md'
@@ -30,54 +29,142 @@ const StyledButton = styled(BaseButton)`
   outline: 0 !important;
   cursor: pointer;
   vertical-align: middle;
-  padding: 5px 24px;
+  padding: 8px 24px;
   font-size: 16px;
-  font-weight: bold;
-  line-height: 22px;
-  color: ${({ primary, theme }) => (primary ? theme.palette.white : theme.palette.black)};
-  background: ${({ primary, theme }) => (primary ? theme.palette.primary : 'transparent')};
-  border: 4px solid ${({ primary, theme }) => (primary ? 'transparent' : theme.palette.black)};
-  transition: background 0.3s;
+  font-weight: ${({ theme }) => theme.font.weight.bold};
+  line-height: 1;
+  color: ${({ type, theme }) => (
+    type === 'primary'
+      ? theme.palette.white
+      : (type === 'ghost' ? hexa(theme.palette.white, 0.8) : theme.palette.black)
+  )};
+  background: ${({ type, theme }) => (type === 'primary' ? theme.palette.primary : 'transparent')};
+  border: 4px solid ${({ type, theme }) => (
+    type === 'primary'
+      ? 'transparent'
+      : (type === 'ghost' ? hexa(theme.palette.white, 0.8) : theme.palette.black)
+  )};
+  transition: all 0.3s;
 
   &:hover {
-    background: ${({ primary, theme }) => (primary ? hexa(theme.palette.primary, 0.8) : theme.palette.daisy)};
+    color: ${({ type, theme }) => (type === 'ghost' ? theme.palette.white : '')};
+    background: ${({ type, theme }) => (
+      type === 'primary'
+        ? hexa(theme.palette.primary, 0.8)
+        : (type === 'ghost' ? 'transparent' : theme.palette.daisy)
+    )};
+    border-color: ${({ type, theme }) => (type === 'ghost' ? theme.palette.white : '')};
   }
 
   &:active {
-    color: ${({ primary, theme }) => (primary ? theme.palette.white : theme.palette.black)};
+    color: ${({ type, theme }) => (type === 'ghost' ? theme.palette.white : '')};
+    background: ${({ type, theme }) => (
+      type === 'primary'
+        ? hexa(theme.palette.primary, 0.6)
+        : (type === 'ghost' ? hexa(theme.palette.white, 0.3) : hexa('#000000', 0.08))
+    )};
+    border-color: ${({ type, theme }) => (type === 'ghost' ? theme.palette.white : '')};
   }
 
   &[disabled] {
-    color: ${({ primary, theme }) => (primary ? theme.palette.white : theme.palette.frost)};
-    background: ${({ primary, theme }) => (primary ? theme.palette.frost : 'transparent')};
-    border-color: ${({ primary, theme }) => (primary ? 'transparent' : theme.palette.frost)};
+    color: ${({ type, theme }) => (
+      type === 'primary'
+        ? theme.palette.white
+        : (type === 'ghost' ? hexa(theme.palette.white, 0.16) : theme.palette.frost)
+    )};
+    background: ${({ type, theme }) => (
+      type === 'primary'
+        ? theme.palette.stone
+        : 'transparent'
+    )};
+    border-color: ${({ type, theme }) => (
+      type === 'primary'
+        ? 'transparent'
+        : (type === 'ghost' ? hexa(theme.palette.white, 0.16) : theme.palette.frost)
+    )};
     cursor: not-allowed;
+  }
 
-    &:hover {
-      background: ${({ primary, theme }) => (primary ? theme.palette.frost : 'transparent')};
-    }
+  div,
+  span {
+    margin-right: 8px;
   }
 `
 
 const StyledButtonSm = StyledButton.extend`
-  padding: 9px 7px;
-  font-size: ${({ theme }) => theme.font.size.sm}px;
-  font-weight: normal;
-  line-height: ${({ theme }) => theme.font.lineHeight.sm}px;
-  border: 1px solid ${({ primary, theme }) => (primary ? 'transparent' : theme.palette.daisy)};
+  padding: 11px 24px;
+  font-weight: ${({ theme }) => theme.font.weight.normal};
+  border: 1px solid ${({ type, theme }) => (
+    type === 'primary'
+      ? 'transparent'
+      : (type === 'ghost' ? hexa(theme.palette.white, 0.8) : theme.palette.daisy)
+  )};
+
+  &:hover {
+    color: ${({ type, theme }) => (type === 'ghost' ? theme.palette.white : '')};
+    background: ${({ type, theme }) => (
+      type === 'primary'
+        ? hexa(theme.palette.primary, 0.8)
+        : 'transparent'
+    )};
+    border-color: ${({ type, theme }) => (
+      type === 'primary'
+        ? 'transparent'
+        : (type === 'ghost' ? theme.palette.white : theme.palette.stone)
+    )};
+  }
+
+  &:active {
+    color: ${({ type, theme }) => (type === 'ghost' ? theme.palette.white : '')};
+    background: ${({ type, theme }) => (
+      type === 'primary'
+        ? hexa(theme.palette.primary, 0.6)
+        : (type === 'ghost' ? hexa(theme.palette.white, 0.3) : 'transparent')
+    )};
+    border-color: ${({ theme }) => theme.palette.black};
+    border-color: ${({ type, theme }) => (
+      type === 'primary'
+        ? 'transparent'
+        : (type === 'ghost' ? theme.palette.white : '')
+    )};
+  }
+
+  &[disabled] {
+    color: ${({ type, theme }) => (
+      type === 'ghost'
+        ? hexa(theme.palette.white, 0.16)
+        : ''
+    )};
+    background: ${({ type, theme }) => (
+      type === 'primary'
+        ? theme.palette.stone
+        : 'transparent'
+    )};
+
+    &:hover {
+      border-color: ${({ type, theme }) => (
+        type === 'primary'
+          ? 'transparent'
+          : (type === 'ghost' ? hexa(theme.palette.white, 0.16) : theme.palette.frost)
+      )};
+    }
+  }
 `
 
 const StyledButtonMd = StyledButton.extend`
-  padding: 6px 24px;
+  padding: 10px 24px;
   font-size: ${({ theme }) => theme.font.size.md}px;
-  line-height: ${({ theme }) => theme.font.lineHeight.md}px;
 `
 
 export default ({ children, size, icon, disabled, loading, ...rest }: Props) => {
   const Button = size === 'sm' ? StyledButtonSm : size === 'md' ? StyledButtonMd : StyledButton
   return (
     <Button disabled={loading || disabled} {...rest}>
-      {icon ? loading ? <Spinner size={16} inline={true} /> : <Icon glyph={icon} size={16} /> : ''}
+      {
+        loading
+          ? <Spinner size={16} inline={true} />
+          : (icon ? <Icon glyph={icon} size={16} /> : null)
+      }
       {children}
     </Button>
   )
