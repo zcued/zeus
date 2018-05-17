0325df1066870a8fdba883218087f3c6993c2eb6
Ma Ning <maning0809@gmail.com>
2018-05-17 15:59:02 +0800

    update: tooltip (#45)

--- site/pages/tooltip/index.tsx
+++ site/pages/tooltip/index.tsx
@@ -4,9 +4,14 @@ import { Tooltip } from '../../../src'
 export default class Page extends React.Component {
   render() {
     return (
-      <Tooltip placement="top" title={<div>alsdjalsdj</div>}>
-        <span>12312123</span>
-      </Tooltip>
+      <React.Fragment>
+        <Tooltip placement="top" title={<div>alsdjalsdj</div>}>
+          <span>12312123</span>
+        </Tooltip>
+        <Tooltip placement="top" title={<a href="/">asdfasdfasdf</a>}>
+          <span>asdfasdfasdf</span>
+        </Tooltip>
+      </React.Fragment>
     )
   }
 }
