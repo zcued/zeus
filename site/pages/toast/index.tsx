d457c1104c2604f296599ad209ffb9b011be319e
cuining <cncc0824@gmail.com>
2018-05-18 18:16:06 +0800

    style(code):

---
+++ site/pages/toast/index.tsx
@@ -0,0 +1,8 @@
+import * as React from 'react'
+import { Toast } from '../../../src'
+
+export default class Page extends React.Component {
+  render() {
+    return <Toast>this is a toast</Toast>
+  }
+}
