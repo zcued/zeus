49445905ca4d5b51f209b3b2a2773c751d708d86
崔宁 <cncc0824@126.com>
2018-05-21 14:22:10 +0800

    fix(controled dropdown): (#50)

--- site/pages/dropdown/index.tsx
+++ site/pages/dropdown/index.tsx
@@ -2,6 +2,14 @@ import * as React from 'react'
 import { Dropdown } from '../../../src'
 
 export default class Page extends React.Component {
+  state = {
+    isOpen: false
+  }
+
+  handleToggle = () => {
+    this.setState({ isOpen: !this.state.isOpen })
+  }
+
   render() {
     return (
       <React.Fragment>
@@ -11,6 +19,11 @@ export default class Page extends React.Component {
         </Dropdown>
 
         <Dropdown text="click" trigger="click">
+          <div>123123</div>
+          <div>456456</div>
+        </Dropdown>
+
+        <Dropdown text="Controled dropdown" isOpen={this.state.isOpen} onClick={this.handleToggle} trigger="click">
           <div>123123</div>
           <div>456456</div>
         </Dropdown>
