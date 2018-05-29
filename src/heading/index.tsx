f1d65d5a81c093036bf4e419d70af37090385d1b
cuining <cncc0824@gmail.com>
2018-05-29 19:05:51 +0800

    sync branch

--- src/heading/index.tsx
+++ src/heading/index.tsx
@@ -10,7 +10,6 @@ interface Props {
 const H: React.SFC<Props> = ({ size = 3, children, ...rest }) => React.createElement(`h${size}`, { ...rest }, children)
 
 const Heading = styled(H)`
-  margin: ${T('spacing.sm')}px;
   color: ${T('palette.black')};
   font-weight: ${T('font.weight.normal')};
   line-height: 1.4;
