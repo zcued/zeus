c5e22ff3a1cf7916c2a3a5822265a4c05b220285
崔宁 <cncc0824@126.com>
2018-05-29 16:31:20 +0800

    refactor(/src): tweak defalut values & styles (#64)

--- src/heading/index.tsx
+++ src/heading/index.tsx
@@ -7,7 +7,7 @@ interface Props {
   size?: 1 | 2 | 3 | 4 | 5 | 6
 }
 
-const H: React.SFC<Props> = ({ size, children, ...rest }) => React.createElement(`h${size}`, { ...rest }, children)
+const H: React.SFC<Props> = ({ size = 3, children, ...rest }) => React.createElement(`h${size}`, { ...rest }, children)
 
 const Heading = styled(H)`
   margin: ${T('spacing.sm') + 'px'};
