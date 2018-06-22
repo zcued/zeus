0cf9495ff630ca71649b90d90299096b2154238e
cuining <cncc0824@gmail.com>
2018-06-22 18:57:33 +0800

    fix(src/datapicker): stylelint

--- src/datepicker/calender.tsx
+++ src/datepicker/calender.tsx
@@ -4,77 +4,85 @@ import styled from '../theme/styled-components'
 const width = '300px'
 
 export const CalenderHeader = styled.div`
-width:100%;
-display:flex;
-justify-content:space-between;
-height:20%;
-align-items:center;
-margin:10px 0 10px 0;
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  height: 20%;
+  align-items: center;
+  margin: 10px 0 10px 0;
 `
 export const CalenderBody = styled.div`
   table {
     width: ${width};
   }
 `
+
 export const Wrapper = styled.div`
-width: ${width};
+  width: ${width};
 `
+
 export const Title = styled.div`
-font-family: Futura - Bold
-font-size: 28px
-color: #282828
-letter-spacing: 0;
+  font-family: Futura, sans-serif;
+  font-size: 28px;
+  color: #282828;
+  letter-spacing: 0;
 `
+
 export const Table = styled.table`
-height:100%;
-border:1pz black;
-  & td{
-    height:42px;
-    outline:none;
-    border:0;
-    padding:0;
+  height: 100%;
+  border: 1px solid black;
+  & td {
+    height: 42px;
+    outline: none;
+    border: 0;
+    padding: 0;
   }
 `
+
 export const Cell = styled.button`
-  width:100%;
-  height:100%;
+  width: 100%;
+  height: 100%;
   background: transparent;
   outline: none;
-  border:0;
-  &.selected{
-    background: #EA4335;
+  border: 0;
+  &.selected {
+    background: #ea4335;
     color: white;
-    border-radius:50%;
+    border-radius: 50%;
   }
-  &:focus{
-    outline:none;
+  &:focus {
+    outline: none;
   }
 `
+
 interface Date {
-  year: number,
-  month: number,
+  year: number
+  month: number
   day: number
 }
 
 interface Props {
-  changeDate: Function,
-  defaultValue?: Date,
+  changeDate: Function
+  defaultValue?: Date
 }
+
 interface State {
-  year: number,
-  month: number,
-  day: number,
-  currentYear: number,
+  year: number
+  month: number
+  day: number
+  currentYear: number
   currentMonth: number
 }
+
 const weekDayNames = ['一', '二', '三', '四', '五', '六', '日']
 
 class Calender extends React.Component<Props, State> {
+  state: State
 
   constructor(props: Props) {
     super(props)
 
-    const { defaultValue } = this.props
+    const { defaultValue } = props
 
     this.state = {
       // 选中的年月日
@@ -85,7 +93,6 @@ class Calender extends React.Component<Props, State> {
       currentYear: defaultValue ? defaultValue.year : new Date().getFullYear(),
       currentMonth: defaultValue ? defaultValue.month : new Date().getMonth()
     }
-
   }
 
   // 当前月各天的时间状态
@@ -100,7 +107,7 @@ class Calender extends React.Component<Props, State> {
     if (dayInWeek > 1) {
       d.setDate(0)
       const lastMonthDaysCount = d.getDate()
-      for (let i = dayInWeek - 1; i--;) {
+      for (let i = dayInWeek - 1; i--; ) {
         dates.push({
           year: d.getFullYear(),
           month: d.getMonth(),
@@ -137,6 +144,7 @@ class Calender extends React.Component<Props, State> {
 
   handleDaySelect(date: any) {
     const { year, month, day } = date
+
     this.setState({
       year,
       month,
@@ -151,6 +159,7 @@ class Calender extends React.Component<Props, State> {
       day
     })
   }
+
   changeDate(change: number, type?: string) {
     const d = new Date(this.state.currentYear, this.state.currentMonth)
     if (type === 'year') {
@@ -166,8 +175,8 @@ class Calender extends React.Component<Props, State> {
       })
     }
   }
-  renderCalender(dates: any[]) {
 
+  renderCalender(dates: any[]) {
     const chunks = []
     for (let i = 0; i < dates.length; i += 1) {
       if (i % 7 === 0) chunks.push([])
@@ -176,36 +185,22 @@ class Calender extends React.Component<Props, State> {
     const { year, month, day } = this.state
     return (
       <>
-        {
-          chunks.map((item, index) => (
-            <tr key={index}>
-              {
-                item.map((date, i) => (
-                  <td key={i}>
-                    {
-                      date.notThisMonth ?
-                        null
-                        :
-                        <Cell
-                          className={
-                            year === date.year &&
-                            month === date.month &&
-                            day === date.day &&
-                            `selected`
-                          }
-                          onClick={this.handleDaySelect.bind(this, date)}
-                        >
-                          {date.day}
-                        </Cell>
-                    }
-
-                  </td>
-                ))
-              }
-
-            </tr>
-          ))
-        }
+        {chunks.map((item, index) => (
+          <tr key={index}>
+            {item.map((date, i) => (
+              <td key={i}>
+                {date.notThisMonth ? null : (
+                  <Cell
+                    className={year === date.year && month === date.month && day === date.day && `selected`}
+                    onClick={this.handleDaySelect.bind(this, date)}
+                  >
+                    {date.day}
+                  </Cell>
+                )}
+              </td>
+            ))}
+          </tr>
+        ))}
       </>
     )
   }
@@ -221,19 +216,11 @@ class Calender extends React.Component<Props, State> {
             size={26}
             onClick={this.changeDate.bind(this, -1, 'year')}
           />
-          <Icon
-            glyph="angle-left"
-            size={26}
-            onClick={this.changeDate.bind(this, -1)}
-          />
+          <Icon glyph="angle-left" size={26} onClick={this.changeDate.bind(this, -1)} />
 
           <Title>{this.state.currentYear}</Title>
           <Title>{this.state.currentMonth + 1}</Title>
-          <Icon
-            size={26}
-            glyph="angle-right"
-            onClick={this.changeDate.bind(this, 1)}
-          />
+          <Icon size={26} glyph="angle-right" onClick={this.changeDate.bind(this, 1)} />
           <Icon
             size={26}
             style={{ transform: 'rotate(270deg)' }}
@@ -245,11 +232,15 @@ class Calender extends React.Component<Props, State> {
         <CalenderBody>
           <Table>
             <thead>
-              <tr>{weekDayNames.map((name, i) => <th style={{ color: '#A3A3A3' }} key={i}>{name}</th>)}</tr>
+              <tr>
+                {weekDayNames.map((name, i) => (
+                  <th style={{ color: '#A3A3A3' }} key={i}>
+                    {name}
+                  </th>
+                ))}
+              </tr>
             </thead>
-            <tbody>
-              {this.renderCalender(dates)}
-            </tbody>
+            <tbody>{this.renderCalender(dates)}</tbody>
           </Table>
         </CalenderBody>
       </Wrapper>
