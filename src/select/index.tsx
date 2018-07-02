1ccddae96f016978e2f1ba99599498e6aa3a96dd
Martin0809 <maning0809@gmail.com>
2018-07-02 15:26:15 +0800

    update: select

--- src/select/index.tsx
+++ src/select/index.tsx
@@ -8,11 +8,68 @@ interface Option {
 }
 
 interface Props {
+  className?: string
   value?: any
   defaultValue?: any
   options?: Array<Option>
+  'aria-describedby'?: string
+  'aria-label'?: string
+  'aria-labelledby'?: string
+  autoFocus?: boolean
+  backspaceRemovesValue?: boolean
+  blurInputOnSelect?: boolean
+  captureMenuScroll?: boolean
+  closeMenuOnSelect?: boolean
+  components?: any
+  controlShouldRenderValue?: boolean
+  delimiter?: string
+  escapeClearsValue?: boolean
+  filterOption?: Function
+  formatGroupLabel?: Function
+  formatOptionLabel?: Function
+  getOptionLabel?: Function
+  getOptionValue?: Function
+  hideSelectedOptions?: boolean
+  id?: string
+  inputValues?: string
+  inputId?: string
+  instanceId?: 'number' | 'string'
+  isClearable?: boolean
+  isDisabled?: boolean
+  isLoading?: boolean
+  isOptionDisabled?: any
+  isOptionSelected?: Function
+  isMulti?: boolean
+  isRtl?: boolean
+  isSearchable?: boolean
+  loadingMessage?: Function
+  minMenuHeight?: number
+  maxMenuHeight?: number
+  menuIsOpen?: boolean
+  menuPlacement?: 'auto' | 'bottom' | 'top'
+  menuPosition?: 'absolute' | 'fixed'
+  menuPortalTarget?: HTMLElement
+  menuShouldBlockScroll?: boolean
+  menuShouldScrollIntoView?: boolean
+  name?: string
+  noOptionsMessage?: Function
+  onBlur?: Function
   onChange?: Function
-  className?: string
+  onFocus?: Function
+  onInputChange?: Function
+  onKeyDown?: Function
+  onMenuOpen?: Function
+  onMenuClose?: Function
+  onMenuScrollToTop?: Function
+  onMenuScrollToBottom?: Function
+  openMenuOnFocus?: boolean
+  openMenuOnClick?: boolean
+  pageSize?: number
+  placeholder?: string
+  screenReaderStatus?: Function
+  styles?: any
+  tabIndex?: string
+  tabSelectsValue?: boolean
 }
 
 class SelectWrapper extends React.Component<Props> {
@@ -60,6 +117,10 @@ const SelectWrapperStyled = styled(SelectWrapper)`
     }
   }
 
+  .react-select__control {
+    cursor: pointer;
+  }
+
   .react-select__value-container {
     padding: 2px 8px;
   }
@@ -93,6 +154,7 @@ const SelectWrapperStyled = styled(SelectWrapper)`
 
   .react-select__option {
     font-size: ${({ theme }) => theme.font.size.sm}px;
+    cursor: pointer;
 
     &.react-select__option--is-focused {
       background: ${({ theme }) => hexa(theme.palette.primary, 0.6)};
