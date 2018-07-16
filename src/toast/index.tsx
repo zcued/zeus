721ae02c1f07d18ddf0b86053e57a6f91a183fa2
Martin0809 <maning0809@gmail.com>
2018-07-16 16:26:13 +0800

    fix

--- src/toast/index.tsx
+++ src/toast/index.tsx
@@ -7,40 +7,25 @@ interface Props {
   children: any
 }
 
-export const ToastWrapper: any = styled.div`
+export const Toast: any = styled.div`
   position: fixed;
   top: 50%;
   left: 50%;
   box-sizing: border-box;
   margin: auto;
+  padding: 10px 40px;
   max-width: 528px;
-  padding-bottom: 200px;
   font-size: ${T('font.size.md')}px;
   font-weight: ${T('font.weight.bold')};
   color: ${T('palette.black')};
   line-height: 28px;
   text-align: center;
+  background: ${T('palette.white')};
+  box-shadow: 0 4px 16px 0 ${T('palette.black16')};
   pointer-events: none;
   z-index: ${zIndex.toast};
   overflow: hidden;
-  transform: translateX(-50%) translateY(-50%);
-
-  span {
-    display: block;
-    padding: 10px 40px;
-    background: ${T('palette.white')};
-    box-shadow: 0 4px 16px 0 ${T('palette.black16')};
-  }
+  transform: translateX(-50%) translateY(calc(-50% - 100px));
 `
-
-class Toast extends React.Component<Props> {
-  render() {
-    return (
-      <ToastWrapper>
-        <span>{this.props.children}</span>
-      </ToastWrapper>
-    )
-  }
-}
 
 export default Toast
