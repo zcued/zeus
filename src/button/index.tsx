4824fef05a000c30239545b2fbcdb1b2a9ad1e91
qiaoming <qiaoming@zcool.com.cn>
2018-07-25 18:24:46 +0800

    button margin修改

--- src/button/index.tsx
+++ src/button/index.tsx
@@ -74,10 +74,13 @@ const StyledButton = styled(BaseButton)`
       type === 'primary' ? 'transparent' : type === 'ghost' ? hexa(theme.palette.white, 0.16) : theme.palette.frost};
     cursor: not-allowed;
   }
+  > div,
+  [data-icon='true'] {
+    margin-right: 8px;
+  }
 
   > div,
   > span {
-    margin-right: 8px;
     vertical-align: middle;
   }
 `
