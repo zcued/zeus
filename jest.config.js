8abce04cd29112ffca18dd8f97ff1b571e96e922
崔宁 <cncc0824@gmail.com>
2018-09-03 13:04:26 +0800

    fix(src/datepicker): placeholder (#159)
    * fix(src/dropdown): button => span
    
    * remove useless props
    
    * fix type error
    
    * docs: add new docs with next.js
    
    * fix(src/button): snapshots
    
    * fix(src/datepicker): placeholder
    
    props `placeholder` => `startPlaceholder` & `endPlaceholder`
    
    * update version
    
    * fix tests

--- jest.config.js
+++ jest.config.js
@@ -7,9 +7,12 @@ module.exports = {
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
   testPathIgnorePatterns: ['/node_modules/', 'lib'],
   transform: {
-    '^.+\\.(ts|tsx)$': './node_modules/ts-jest/preprocessor.js'
+    '^.+\\.(ts|tsx)$': 'ts-jest'
   },
   testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$',
   testEnvironment: 'node',
+  snapshotSerializers: ['enzyme-to-json/serializer'],
+  setupTestFrameworkScriptFile: './node_modules/jest-enzyme/lib/index.js',
+  unmockedModulePathPatterns: ['react', 'enzyme', 'jest-enzyme'],
   setupFiles: ['./tests/setup.js']
 }
