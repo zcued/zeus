02189d9f6e0d4522dd2464da6669e11f342407bf
Martin0809 <maning0809@gmail.com>
2018-12-19 11:02:30 +0800

    update

--- site/pages/tooltip/index.tsx
+++ site/pages/tooltip/index.tsx
@@ -20,7 +20,12 @@ export default class Page extends React.Component {
   render() {
     return (
       <React.Fragment>
-        <Tooltip placement="top" title={<Title>顶部默认显示</Title>} overlayReset={true} defaultHovering={true}>
+        <Tooltip
+          placement="top"
+          title={<Title>顶部默认显示</Title>}
+          overlayReset={true}
+          defaultHovering={this.state.isHovering}
+        >
           <span>顶部默认显示</span>
         </Tooltip>
         <Tooltip placement="top" title={<a href="/">顶部</a>}>
